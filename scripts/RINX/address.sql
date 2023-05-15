e247416091cc08864fa1257a3612ea562f17f44e
WillhKessler <37121681+WillhKessler@users.noreply.github.com>
2023-05-15 11:48:25 -0400

    Update address.sql

--- scripts/RINX/address.sql
+++ scripts/RINX/address.sql
@@ -1,7 +1,7 @@
 Drop table address If Exists;
 
 Create table addresses (Id character varying(255), longitude float, latitude float, start_date character varying(255), end_date character varying(255));
-copy addresses from '/n/holyscratch01/cga/dkakkar/data/BIL/Input_addresses.csv' (FORMAT csv, HEADER, DELIMITER ',');
+copy addresses from '/pc/n3mhs00/Cindy/For_WilliamK/RawGPSDataRinx.csv' (FORMAT csv, HEADER, DELIMITER ',');
 Alter table addresses add column startdate date;
 Alter table addresses add column enddate date;
 Update addresses set startdate=TO_DATE(start_date,'YYYYMMDD');
