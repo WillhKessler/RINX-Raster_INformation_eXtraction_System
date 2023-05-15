67fee289f1c3f7b36e851be567760ad4e82da65b
WillhKessler <37121681+WillhKessler@users.noreply.github.com>
2023-05-15 11:36:08 -0400

    Update tmax.sh

--- scripts/RINX/tmax.sh
+++ scripts/RINX/tmax.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dir in /pc/nhair0a/Built_Environment/BE_Data/Geographic_Data/PRISM_daily/PRISM_data/an/ppt/daily/*; # Change to your directory, 
+for dir in /pc/nhair0a/Built_Environment/BE_Data/Geographic_Data/PRISM_daily/PRISM_data/an/tmax/daily/*; # Change to your directory, 
 do
     #echo "$dir"
     #dir1=/pc/n3mhs00/Cindy/For_WilliamK # Change the output directory
@@ -8,10 +8,10 @@ do
     read -ra ADDR <<< "$dir" # str is read into an array as tokens separated by IFS
     i="${ADDR[-1]}"
     echo "$i"
-    cmd1="raster2pgsql -d -I -C -M -F -t 100x100 -s 4269 $dir/*.bil ppt$i > ppt$i.sql" # May need to change the 4269 coordinate system, 
+    cmd1="raster2pgsql -d -I -C -M -F -t 100x100 -s 4269 $dir/*.bil tmax$i > tmax$i.sql" # May need to change the 4269 coordinate system, 
     #echo "$cmd1"
     eval "$cmd1"
-    cmd2="psql -h localhost -p 7779 -d postgres -f ppt$i.sql"
+    cmd2="psql -h localhost -p 7779 -d postgres -f tmax$i.sql"
     #echo "$cmd2"
     eval "$cmd2"
     cmd3="rm *.sql"
