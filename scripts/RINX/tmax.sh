defd39ce15ea614945c9fa8191c2da88e9670a52
WillhKessler <37121681+WillhKessler@users.noreply.github.com>
2023-05-15 10:12:48 -0400

    Update tmax.sh

--- scripts/RINX/tmax.sh
+++ scripts/RINX/tmax.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dir in /n/holyscratch01/cga/jblossom/PRISM_data/tmax/daily/*;
+for dir in /n/holyscratch01/cga/jblossom/PRISM_data/tmax/daily/*; # Change to our directory, 
 do
     #echo "$dir"
     #dir1=/n/holyscratch01/cga/dkakkar/data/BIL/output
@@ -8,7 +8,7 @@ do
     read -ra ADDR <<< "$dir" # str is read into an array as tokens separated by IFS
     i="${ADDR[-1]}"
     echo "$i"
-    cmd1="raster2pgsql -d -I -C -M -F -t 100x100 -s 4269 $dir/*.bil tmax$i > tmax$i.sql"
+    cmd1="raster2pgsql -d -I -C -M -F -t 100x100 -s 4269 $dir/*.bil tmax$i > tmax$i.sql" # May need to change the 4269 coordinate system, 
     #echo "$cmd1"
     eval "$cmd1"
     cmd2="psql -h localhost -p 7779 -d postgres -f tmax$i.sql"
